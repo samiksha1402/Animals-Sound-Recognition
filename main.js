96233cb60c8e6d65e2463bff2dd20c7312b1fb61
samiksha1402 <80887833+samiksha1402@users.noreply.github.com>
2022-03-28 14:33:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function start()
+{
+navigator.mediaDevices.getUserMedia({audio:true});
+classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/sS-c0cYz8/model.json', modelReady);
+}
+function modelReady(){
+    console.log("modelReady");
+    classifier.classify(gotResults);
+}
+function gotResults(error,results){
+    if (error){
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        r = Math.floor(Math.random() * 255) + 1;
+        g = Math.floor(Math.random() * 255) + 1;
+        b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("label").innerHTML="I can hear - " + results[0].label;
+        document.getElementById("confidence").innerHTML="Accuracy - " + (results[0].confidence * 100).toFixed(2) + "%";
+        document.getElementById("label").style.color= "rgb("+ r + ", " + g +", "+ b +")";
+        document.getElementById("confidence").style.color= "rgb("+ r + ", " + g +", "+ b +")";
+   }
+}
+
